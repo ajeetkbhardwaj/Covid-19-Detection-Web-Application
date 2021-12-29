f5b06f55df60794e9b8651f793c1e733aa3d644b
Ajeet Kumar Bhardwaj <87787220+ajeetkbhardwaj@users.noreply.github.com>
2021-12-29 18:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def upload_image():
         return render_template('home.html', prediction = 'Invalid File extension')
 # return the prediction to above templete
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_reloader=False)
